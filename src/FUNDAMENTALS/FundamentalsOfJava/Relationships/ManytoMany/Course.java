59cddde2b4c250b7c2bb7c7d16e8f44a61be0f32
Gabriel-F-Nobre <nobrecoder@gmail.com>
2023-07-03 18:44:30 -0300

    03/07/2023

--- src/FUNDAMENTALS/FundamentalsOfJava/Relationships/ManytoMany/Course.java
+++ src/FUNDAMENTALS/FundamentalsOfJava/Relationships/ManytoMany/Course.java
@@ -1,22 +1,28 @@
 package FUNDAMENTALS.FundamentalsOfJava.Relationships.ManytoMany;
 import java.util.ArrayList;
 
+//Note como a implementação de N:N foi feita nessa classe...
+
 public class Course {
 
-    String name;
-    ArrayList<Student> students = new ArrayList<>();
+    String name;    //Nosso objeto recebe um nome que servirá para identificá-lo
+    ArrayList<Student> students = new ArrayList<>();    //Um ArrayList para guardar muitos objetos para
+                                                        //um único objeto dessa classe...
 
     Course(String name){
         this.name = name;
     }
 
-    void addStudent(Student student){
-        this.students.add(student);
-        student.courses.add(this);
-    }
+    void addStudent(Student student){   //Esse é o método que usamos para atribuír novos objetos a
+        this.students.add(student);     //classe Students, note que ele recebe um objeto de student,
+        student.courses.add(this);      //esse objeto não só cria novas instâncias em Student como
+    }                                   //também usa o "this" para referenciar um objeto de
+                                        //Course lá na classe Student. Isso que faz com que a classe
+                                        //Student entenda que aqueles muitos objetos instaciados á
+                                        //ela pertencem a um único objeto de Course...
 
-    void showMeStudentsThatCourse(){
-        for(Student student: this.students){
+    void showMeStudentsThatCourse(){                //Esse método mostrará somente os objetos dependentes
+        for(Student student: this.students){        //que pertencem ao objeto em questão graças ao "this"...
             System.out.println(student.name);
         }
     }
--- src/FUNDAMENTALS/FundamentalsOfJava/Relationships/ManytoMany/Student.java
+++ src/FUNDAMENTALS/FundamentalsOfJava/Relationships/ManytoMany/Student.java
@@ -1,22 +1,28 @@
 package FUNDAMENTALS.FundamentalsOfJava.Relationships.ManytoMany;
 import java.util.ArrayList;
 
+//Note como a implementação de N:N foi feita nessa classe...
+
 public class Student {
 
-    String name;
-    ArrayList<Course> courses = new ArrayList<>();
+    String name;    //Nosso objeto recebe um nome que servirá para identificá-lo
+    ArrayList<Course> courses = new ArrayList<>();  //Um ArrayList para guardar muitos objetos para
+                                                    //um único objeto dessa classe...
 
     Student(String name){
         this.name = name;
     }
 
-    void addCourse(Course course){
-        this.courses.add(course);
-        course.students.add(this);
-    }
+    void addCourse(Course course){  //Esse é o método que usamos para atribuír novos objetos a
+        this.courses.add(course);   //classe Courses, note que ele recebe um objeto de course
+        course.students.add(this);  //esse objeto não só cria novas instâncias em Course como
+    }                               //também usa o "this" para referenciar um objeto de
+                                    //Student lá na classe Course. Isso que faz com que a classe
+                                    //Course entenda que aqueles muitos objetos instaciados á
+                                    //ela pertencem a um único objeto de Student...
 
-    void showMeCoursesThatStudent(){
-        for(Course course: this.courses){
+    void showMeCoursesThatStudent(){            //Esse método mostrará somente os objetos dependentes
+        for(Course course: this.courses){       //que pertencem ao objeto em questão graças ao "this"...
             System.out.println(course.name);
         }
     }
