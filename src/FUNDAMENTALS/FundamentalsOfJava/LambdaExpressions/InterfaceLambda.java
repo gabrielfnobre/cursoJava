c81f91ed1ace85ce5241a4ee3155cd2df28f65d4
Gabriel <nobrecoder@gmail.com>
2023-07-28 13:45:21 -0300

    28/07/2023

---
+++ src/FUNDAMENTALS/FundamentalsOfJava/LambdaExpressions/InterfaceLambda.java
@@ -0,0 +1,10 @@
+package FUNDAMENTALS.FundamentalsOfJava.LambdaExpressions;
+
+/*  Aqui temos uma interface comum, como geralmente usamos...
+* */
+
+public interface InterfaceLambda {
+    Integer execute(int a, int b);  //Como sempre, o objeto é gerar um método abstrato que deverá ser implementado de
+}                                   // "n" formas numa classe concreta...
+
+//VÁ AGORA PARA A CLASSE LambdaExpressions...
