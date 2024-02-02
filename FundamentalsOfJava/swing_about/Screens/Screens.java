2169de18a585826ba6510dbb7fe2cced7202350b
gabriel <nobrecoder@gmail.com>
2024-02-02 05:48:30 -0300

    02/02/2024

--- FundamentalsOfJava/swing_about/Screens/Screens.java
+++ FundamentalsOfJava/swing_about/Screens/Screens.java
@@ -21,32 +21,32 @@ import javax.swing.JPanel;
 public class Screens {
 	public static void main(String[] args) {
 		
-		JFrame screen = new JFrame("My Subject"); // 1º criamos um frame (janela), a janela é o "Subject" no swing... 
+		JFrame screen = new JFrame("My Subject"); // 1º criamos um frame (janela), onde vamos colocar todos os componentes visuais... 
 		screen.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Faz a execução parar quando clicamos no "X" para fechar a janela
 		screen.setSize(600, 600); //Dimensiona o tamanho da janela
 		screen.setLayout(new FlowLayout()); //Define o layout como FlowLayout para que os componentes não peguem todo o tamanho da tela
 		screen.setLocationRelativeTo(null); //Define a posição em que a janela aparece como o centro do monitor
 		
-		JButton button = new JButton("I'm Observer Concrete"); //O botão funciona como um Observer Concrete, que quer saber quando for clicado
+		JButton button = new JButton("Change the Text!!!"); //O botão funciona como um Subject, que quer saber quando o evento de click acontece
 		button.setPreferredSize(new Dimension(550, 50)); //Dimensionamento do botão
-		screen.add(button); //Botão é registrado no subject, ou seja, a janela...
+		screen.add(button); //Botão é adicionado a janela
 		
-		JPanel painel = new JPanel(); //Painel é outro Observer Concrete
-		screen.add(painel); //O Painel é registrado no subject também...
+		JPanel painel = new JPanel(); //Criamos outro componente visual, o painel que pode receber elementos dentro dele
+		screen.add(painel); //O Painel é adicionado a tela...
 		painel.setBackground(Color.PINK); //Mudando a cor do painel...
 		painel.setPreferredSize(new Dimension(200, 150)); //Dimensionando o painel...
 		
 		JLabel labelText = new JLabel(); //O texto é criado
-		painel.add(labelText); //Texto é registrado no painel, não na tela...
+		painel.add(labelText); //Texto adicionado ao painel rosa...
 		
-		button.addActionListener(e -> { //Aqui temos o identificador do evento, quando o evento ocorre, a lambda é executada, alternando o texto
-			String text = labelText.getText(); //do painel, sempre que o botão é clicado.
+		button.addActionListener(e -> { //Aqui temos padrão observer implementado, quando o botão identifica o clica, ele fará um comportamento na letra do painel.
+			String text = labelText.getText(); //que será alternada a cada click.
 			if(text.equalsIgnoreCase("OOOOH")) {				
 				labelText.setText("AAAAH");
 			} else {
 				labelText.setText("OOOOH");
 			}
-		}); //Quando o botão recebe um clic
+		});
 		
 		screen.setVisible(true);
 	}
