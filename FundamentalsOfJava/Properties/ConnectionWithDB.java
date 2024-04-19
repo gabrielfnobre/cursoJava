a6c4e5a007076921c5e2751d766e7516ba8bbd14
Gabriel-F-Nobre <nobrecoder@gmail.com>
2024-04-19 05:38:04 -0300

    19/04/2024

--- FundamentalsOfJava/Properties/ConnectionWithDB.java
+++ FundamentalsOfJava/Properties/ConnectionWithDB.java
@@ -8,37 +8,46 @@ import java.sql.Statement;
 import java.util.Properties;
 
 public class ConnectionWithDB {
+	//Veja que na nossa classe main pegamos o arquivo .properties por através do mé-
+	//todo da classe "GenerateProperties" e usando o método "testingConnection" vamos
+	//capturar os valores do arquivo ".properties" e fazer um teste de acesso ao ban-
+	//co de dados.
 	public static void main(String[] args) {
 		
 		Properties prop = GenerateProperties.createProperties();
 		testingConnection(prop);
 	}
 	
+	//Esse método estabelece a lógica de captura e acesso ao banco de dados por a-
+	//través de um arquivo ".properties"...
 	private static void testingConnection(Properties prop) {
-		final String url = prop.getProperty("db.url");
-			final String user = prop.getProperty("db.user");
-			final String password = prop.getProperty("db.password");
+		final String url = prop.getProperty("db.url"); //Capturando valor a partir da chave para a url...
+		final String user = prop.getProperty("db.user"); //Capturando valor a partir da chave para user...
+		final String password = prop.getProperty("db.password"); //Capurando valor a partir da chave para password...
 			
-			try {
-				System.out.println("Open connection with Data Base...");
-				Connection connection = DriverManager.getConnection(url, user, password);
-				
-				Statement stmt = connection.createStatement();
-				String sql = "SELECT * FROM estados";
-				ResultSet result = stmt.executeQuery(sql);
-				
-				while(result.next()){
-					System.out.println(result.getInt("id") + " - "
-							+ result.getString("nome") + " | "
-							+ result.getString("sigla") + " | "
-							+ result.getString("regiao") + " | "
-							+ result.getFloat("populacao"));
-				}
-				
-				connection.close();
-				
-			} catch (SQLException e) {
-				e.printStackTrace();
+		//O try catch trata erros de conexão com banco de dados...
+		try {
+			System.out.println("Open connection with Data Base...");
+			Connection connection = DriverManager.getConnection(url, user, password); //Gerando conexão a partir dos valores armazenados...
+			
+			//Passando comandos SQL ao banco MySQL e recebendo dados...
+			Statement stmt = connection.createStatement();
+			String sql = "SELECT * FROM estados";
+			ResultSet result = stmt.executeQuery(sql);
+			
+			//Mostrando dados recebidos para provar que a conexão aconteceu...
+			while(result.next()){
+				System.out.println(result.getInt("id") + " - "
+						+ result.getString("nome") + " | "
+						+ result.getString("sigla") + " | "
+						+ result.getString("regiao") + " | "
+						+ result.getFloat("populacao"));
 			}
+				
+			connection.close(); //Encerrando conexão...
+				
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- FundamentalsOfJava/Properties/GenerateProperties.java
+++ FundamentalsOfJava/Properties/GenerateProperties.java
@@ -6,28 +6,34 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.Scanner;
 
+//	Veja a explicação da aula após o código abaixo...
+
 public class GenerateProperties {
-	final static String directory = System.getProperty("user.home");
-	final static String folderName = ".test_for_properties";
-	final static String archiveName = ".connectionDB";
-	final static String url = "jdbc:mysql://localhost:3306/wm?verifyServerCertificate=false&useSSL=true";
-	static String user;
-	static String password;
+	//Abaixo estão variáveis responsáeis por dar nome ao nosso arquivo .properties
+	//bem como a pasta onde ele será armazenado e aos valores que iremos armazenar
+	//dentro dele...
+	final static String directory = System.getProperty("user.home"); //referencia ao diretório de usuário...
+	final static String folderName = ".test_for_properties"; //Esse será o nome da pasta oculta
+	final static String archiveName = ".connectionDB"; //Esse será o nome do arquivo .properties oculto
+	final static String url = "jdbc:mysql://localhost:3306/wm?verifyServerCertificate=false&useSSL=true"; //url do banco de dados...
+	static String user; //login de acesso ao banco de dados...
+	static String password; //senha de acesso
 	
+	//Esse método executa a lógica de criação do nosso arquivo ".properties"...
 	public static Properties createProperties() {		
-		File folder = createFolder();
-		setUserPassword();
+		File folder = createFolder(); //Armazenando informações da pasta que foi criada para guardar o arquivo...
+		setUserPassword(); //Capturando o login e senha do usuário para poder armazenar...
 		
-		Properties properties = new Properties();
-		properties.setProperty("db.url", url);
-		properties.setProperty("db.user", user);
-		properties.setProperty("db.password", password);
+		Properties properties = new Properties(); //Criado um objeto que irá gerar o arquivo ".properties"...
+		properties.setProperty("db.url", url); //armazenando dados da url no objeto Properties...
+		properties.setProperty("db.user", user); //armazenando dados do login no objeto Properties...
+		properties.setProperty("db.password", password); //armazenando dados da senha no objeto Properties...
 		
-		File propertiesArchive = new File(folder, archiveName);
+		File propertiesArchive = new File(folder, archiveName); //Gerando caminho de criação do arquivo .properties...
 		
-		try (FileWriter writer = new FileWriter(propertiesArchive)) {
-			properties.store(writer, "Archive generate");
-			System.out.println("Arquivo .properties oculted is created on: " + propertiesArchive.getAbsolutePath());
+		try (FileWriter writer = new FileWriter(propertiesArchive)) { //Gerando objeto que irá armazenar o arquivo na pasta...
+			properties.store(writer, "Archive generate"); //Armazenando arquivo na pasta e infomação adicional em forma de comentário no arquivo...
+			System.out.println("Arquivo .properties oculted is created on: " + propertiesArchive.getAbsolutePath()); //Mostrando path do arquivo...
 		} catch (IOException e) {
 			System.out.println("Error to generate properties");
 			e.printStackTrace();
@@ -36,6 +42,7 @@ public class GenerateProperties {
 		return properties;
 	}
 	
+	//Esse método executa a lógica de criação da nossa pasta oculta 
 	public static File createFolder() {
 		File folder = new File(directory, folderName);
 		
@@ -49,6 +56,7 @@ public class GenerateProperties {
 		}
 	}
 	
+	//Esse método executa a lógica de captura o login e senha por parte do usuário...
 	public static void setUserPassword() {
 		Scanner into = new Scanner(System.in);
 		
@@ -64,5 +72,48 @@ public class GenerateProperties {
 		into.close();
 	}
 	
+	//Após isso vá até a classe "ConnectionWithDB para ver como fizemos para capturar
+	//os valores a partir de um arquivo ".properties" e passar esses valores para o 
+	//JDBC para acessarmos ao banco de dados e usarmos esses valores...
 	
 }
+
+/*	PROPERTIES
+
+	Nesta aula vamos aprender como o que são e como usar arquivos ".properties" pa-
+	ra auxiliar na segurança das nossas aplicações quando lidamos com dados sensíve-
+	is como informações de login e senha.
+	
+	Arquivos .properties são arquivos textuais que podem ser armazenados na nossa 
+	própria máquina ou na máquina dos usuários para guardar informações importantes 
+	como "login" e "senha" de aplicações. Geralmente esses arquivos ficam ocultos ou 
+	armazenados dentro de uma pasta oculta na pasta padrão de usuários nos sistemas 
+	operacionais, mas podemos escondê-la onde nós desejarmos.
+	
+	Arquivos .properties possuem uma estrutura chave valor (assim como o json) onde 
+	armazenam informações importantes que podem ser resgatadas pela classe "Properti-
+	es" de java.util.
+	
+	A sintaxe de um arquivo .properties é a seguinte...
+	
+		chave=valor
+		
+	Onde chave é o nome o identificador do valor que desejamos resgatar e valor é o
+	próprio valor que será resgatado.
+	
+	Podemos colocar comentários também usando "#" da seguinte forma:
+	
+		#comentários...
+		
+	Qualquer texto que tiver uma linha como prefixo é um comentário.
+	
+	Veja no exemplo acima como usamos o java tanto para gerar um arquivo ".properti-
+	es" como para escondê-lo dentro de uma pasta oculta e capturar os valores obti-
+	dos a partir dele...
+
+*/
+
+
+
+
+
