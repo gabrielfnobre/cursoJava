2782243f3dd0d5953a5631aa0876475ef739233e
gabriel <nobrecoder@gmail.com>
2024-05-27 11:34:00 -0300

    27/05/2024

--- FundamentalsOfJava/JPA/src/model/user_example/Inheritance/SINGLE_TABLE/ScholarshipHolder.java
+++ FundamentalsOfJava/JPA/src/model/user_example/Inheritance/SINGLE_TABLE/ScholarshipHolder.java
@@ -4,21 +4,38 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+/* E aqui temos a implementação da nossa classe ScholarshipHolder para simular os alunos bolsistas na relação de 
+ * herança da estratégia SINGLE_TABLE. Veja como fizemos a implementação para identificá-la no Java...
+ * */
+
+//Primeiro a mapeamos a uma entidade do Banco de Dados...
 @Entity
 @Table(name = "scholarship_holder")
+//Como estamos usando uma coluna Discriminator na nossa classe Mãe, temos que gerar um valor descriminador na co-
+//luna descriminadora, que no nosso caso é a coluna "type". Nós definimos a sigla "SH" como descriminadora de to-
+//das as ocorrências que forem do tipo aluno bolsista...
 @DiscriminatorValue("SH")
+//Fizemos nossa classe extender de Student...
 public class ScholarshipHolder extends Student {
 
+	//Note que geramos um atributo a mais para a nossa classe ScholarshipHolder, esse atributo irá receber o valor
+	//da bolsa que o aluno irá receber...
 	private int scholarship;
 	
+	//Temos um construtor padrão como é mandatório para as classes mapeadas pelo JPA...
 	public ScholarshipHolder() {}
 
+	//E temos um construtor customizado, onde vamos pegar o valor de registro do aluno, seu nome e valor de bolsa
+	//que ele vai receber...
 	public ScholarshipHolder(Long registration, String name, int scholarship) {
 		super();
 		this.setRegistration(registration);
 		this.setName(name);
 		this.scholarship = scholarship;
 	}
+	
+	//Como já temos métodos getters and setters herdados da nossa classe mãe, precisamos definir apenas os getters
+	//and setters dos atributos gerados somente na classe filha...
 
 	public int getScholarship() {
 		return scholarship;
--- FundamentalsOfJava/JPA/src/model/user_example/Inheritance/SINGLE_TABLE/Student.java
+++ FundamentalsOfJava/JPA/src/model/user_example/Inheritance/SINGLE_TABLE/Student.java
@@ -7,27 +7,42 @@ import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
-/* Note aqui que usamos apenas algumas annotations para simular a herança da nossa classe para o Banco de Dados
+/* Note aqui que usamos apenas algumas annotations para simular a herança entre a nossa classe e o Banco de Dados
+ * usando a estratégia de SINGLE_TABLE, ou seja, onde sabemos que vamos usar apenas 1 única tabela para simular o 
+ * relacionamento de herança. Veja como fazemos ela acontecer...
  * */
 
+//Começamos designando a nossa classe como uma entidade do banco...
 @Entity
+//Designamos a estratégia de herança como "SINGLE_TABLE"...
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+//Devemos escolher uma coluna que irá fazer a descriminação entre os valores que pertencem a classe pai e os valo-
+//res que pertencem as classes filhas, no nosso caso definimos que a coluna "type" irá fazer a distinção entre a-
+//lunos bolsistas e não bolsistas...
 @DiscriminatorColumn(name = "type")
+//Podemos também delegar a classe um valor diferenciador característico aos objetos de uma determinada classe, a-
+//qui definimos que os objetos de Student receberão a sigla "ST" na coluna para identificarmos que as ocorrências
+//pertencem a classe "Student"...
 @DiscriminatorValue("ST")
 public class Student {
 
+	//Definimos que a coluna "registration" será a nossa PK...
 	@Id
 	private Long registration;
 	private String name;
 	
+	//Temos um método construtor Student padrão, como é mandatório para as classes mapeadas no JPA...
 	public Student() {}
 
+	//E temos um método construtor criado por nós para receber o número de registro e o nome dos aluno novos...
 	public Student(Long registration, String name) {
 		super();
 		this.registration = registration;
 		this.name = name;
 	}
 
+	//E abaixo nossos métodos getters and setters...
+	
 	public Long getRegistration() {
 		return registration;
 	}
